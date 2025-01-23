a3256204903be6bf30948c91f009df62efcccef6
Jordi Orts <38767974+jorts64@users.noreply.github.com>
2025-01-23 10:44:47 +0100

    Create kitshields.py

---
+++ lib/kitshields.py
@@ -0,0 +1,18 @@
+from micropython import const
+from machine import Pin
+
+# Pin Assignments
+
+# SPI
+SPI_MOSI = const(38)
+SPI_MISO = const(39)
+SPI_CLK = const(40)
+
+# I2C
+I2C_SDA = const(12)
+I2C_SCL = const(11)
+
+# PIR
+PIN_PIR = const(13)
+PIR = Pin(PIN_PIR, Pin.IN, Pin.PULL_UP)
+
